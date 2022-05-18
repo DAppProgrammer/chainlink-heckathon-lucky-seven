7a6316d169c4d6481397edb9a826cd2dc7c29142
Paramjeet Singh <dapp.programmer@gmail.com>
2022-05-19 00:21:22 +0530

    Updated Dice component

--- pages/components/Dice.jsx
+++ pages/components/Dice.jsx
@@ -76,133 +76,143 @@ const Dice = () => {
   };
 
   return (
-    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center">
-      <div className="w-1/2 h-2/3 text-center text-white pt-14 bg-gradient-to-r from-gray-500 to-gray-900 rounded-2xl">
-        <div className="text-gray-500 text-2xl">
-          Roll the dice to play the game
-        </div>
-
-        <div className="flex justify-center gap-20 m-10">
-          <div id="dice" className="dice1">
-            <div className="side one">
-              <span className="dot"></span>
-            </div>
-            <div className="side two">
-              <span className="dot"></span>
-              <span className="dot"></span>
-            </div>
-            <div className="side three">
-              <span className="dot"></span>
-              <span className="dot"></span>
-              <span className="dot"></span>
-            </div>
-            <div className="side four">
-              <div className="kolona">
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
-              <div className="kolona">
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
-            </div>
-            <div className="side five">
-              <div className="kolona">
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
-              <div className="kolona">
-                <span className="dot"></span>
-              </div>
-              <div className="kolona">
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
-            </div>
-            <div className="side six">
-              <div className="kolona">
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
-              <div className="kolona">
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
-              <div className="kolona">
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
-            </div>
-          </div>
-
-          <div id="dice2" className="dice2">
-            <div className="side one">
-              <span className="dot"></span>
-            </div>
-            <div className="side two">
-              <span className="dot"></span>
-              <span className="dot"></span>
-            </div>
-            <div className="side three">
-              <span className="dot"></span>
-              <span className="dot"></span>
-              <span className="dot"></span>
-            </div>
-            <div className="side four">
-              <div className="kolona">
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
-              <div className="kolona">
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
-            </div>
-            <div className="side five">
-              <div className="kolona">
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
-              <div className="kolona">
-                <span className="dot"></span>
-              </div>
-              <div className="kolona">
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
-            </div>
-            <div className="side six">
-              <div className="kolona">
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
-              <div className="kolona">
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
-              <div className="kolona">
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div>
-          {!loading && !executed && (
+    <>
+      <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center">
+        <div className="w-1/2 h-2/3  text-white pt-1 bg-gradient-to-r from-gray-500 to-gray-900 rounded-2xl justify-end text-right">
+          {executed ? (
             <button
-              className="cursor-pointer px-3 py-2 m-3 items-center flex-col rounded-full my-5 border-2  "
-              onClick={rollTheDice}
+              onClick={() => setApproved(false)}
+              className="p-1 rounded-sm m-3 bg-red-400 text-{#44253e}"
             >
-              Roll the Dice
+              X
             </button>
+          ) : (
+            <div onClick={() => setApproved(false)} className="p-1 m-3 ">
+              &nbsp;
+            </div>
           )}
-          {executed && (
-            <button onClick={() => setApproved(false)}>Play again</button>
-          )}
-          {loading && <Loader />}
+          <div className="text-center items-center">
+            <div className="flex justify-center gap-20 m-10">
+              <div id="dice" className="dice1">
+                <div className="side one">
+                  <span className="dot"></span>
+                </div>
+                <div className="side two">
+                  <span className="dot"></span>
+                  <span className="dot"></span>
+                </div>
+                <div className="side three">
+                  <span className="dot"></span>
+                  <span className="dot"></span>
+                  <span className="dot"></span>
+                </div>
+                <div className="side four">
+                  <div className="kolona">
+                    <span className="dot"></span>
+                    <span className="dot"></span>
+                  </div>
+                  <div className="kolona">
+                    <span className="dot"></span>
+                    <span className="dot"></span>
+                  </div>
+                </div>
+                <div className="side five">
+                  <div className="kolona">
+                    <span className="dot"></span>
+                    <span className="dot"></span>
+                  </div>
+                  <div className="kolona">
+                    <span className="dot"></span>
+                  </div>
+                  <div className="kolona">
+                    <span className="dot"></span>
+                    <span className="dot"></span>
+                  </div>
+                </div>
+                <div className="side six">
+                  <div className="kolona">
+                    <span className="dot"></span>
+                    <span className="dot"></span>
+                  </div>
+                  <div className="kolona">
+                    <span className="dot"></span>
+                    <span className="dot"></span>
+                  </div>
+                  <div className="kolona">
+                    <span className="dot"></span>
+                    <span className="dot"></span>
+                  </div>
+                </div>
+              </div>
+
+              <div id="dice2" className="dice2">
+                <div className="side one">
+                  <span className="dot"></span>
+                </div>
+                <div className="side two">
+                  <span className="dot"></span>
+                  <span className="dot"></span>
+                </div>
+                <div className="side three">
+                  <span className="dot"></span>
+                  <span className="dot"></span>
+                  <span className="dot"></span>
+                </div>
+                <div className="side four">
+                  <div className="kolona">
+                    <span className="dot"></span>
+                    <span className="dot"></span>
+                  </div>
+                  <div className="kolona">
+                    <span className="dot"></span>
+                    <span className="dot"></span>
+                  </div>
+                </div>
+                <div className="side five">
+                  <div className="kolona">
+                    <span className="dot"></span>
+                    <span className="dot"></span>
+                  </div>
+                  <div className="kolona">
+                    <span className="dot"></span>
+                  </div>
+                  <div className="kolona">
+                    <span className="dot"></span>
+                    <span className="dot"></span>
+                  </div>
+                </div>
+                <div className="side six">
+                  <div className="kolona">
+                    <span className="dot"></span>
+                    <span className="dot"></span>
+                  </div>
+                  <div className="kolona">
+                    <span className="dot"></span>
+                    <span className="dot"></span>
+                  </div>
+                  <div className="kolona">
+                    <span className="dot"></span>
+                    <span className="dot"></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              {!loading && !executed && (
+                <button
+                  className="cursor-pointer px-3 py-2 m-3 items-center flex-col rounded-full my-5 border-2  "
+                  onClick={rollTheDice}
+                >
+                  Roll the Dice
+                </button>
+              )}
+
+              {loading && <Loader />}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
